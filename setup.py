4e6ee9b7248518040d6232c426ae584f1a85bfae
Roberto Prevato <roberto.prevato@gmail.com>
2020-02-22 22:55:36 +0100

    improves code :cake: (#3)

--- setup.py
+++ setup.py
@@ -2,27 +2,30 @@ from setuptools import setup
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
-setup(name='roconfiguration',
-      version='1.0.7',
-      description='Implementation of key-value pair based configuration for Python applications.',
-      long_description=readme(),
-      long_description_content_type='text/markdown',
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3',
-          'Operating System :: OS Independent'
-      ],
-      url='https://github.com/RobertoPrevato/roconfiguration',
-      author='RobertoPrevato',
-      author_email='roberto.prevato@gmail.com',
-      keywords='configuration core yaml ini json environment',
-      license='MIT',
-      packages=['roconfiguration'],
-      install_requires=['PyYAML'],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name="roconfiguration",
+    version="1.0.8",
+    description="Implementation of key-value pair based "
+    "configuration for Python applications.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/RobertoPrevato/roconfiguration",
+    author="RobertoPrevato",
+    author_email="roberto.prevato@gmail.com",
+    keywords="configuration core yaml ini json environment",
+    license="MIT",
+    packages=["roconfiguration"],
+    install_requires=["PyYAML"],
+    include_package_data=True,
+    zip_safe=False,
+)
