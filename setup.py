574530a608267be71f52486bd4d0386a5e0c4fff
Roberto Prevato <roberto.prevato@gmail.com>
2021-05-23 12:04:35 +0200

    migrates to GitHub Actions

--- setup.py
+++ setup.py
@@ -8,24 +8,31 @@ def readme():
 
 setup(
     name="roconfiguration",
-    version="1.0.8",
-    description="Implementation of key-value pair based "
-    "configuration for Python applications.",
+    version="1.0.9",
+    description=(
+        "Implementation of key-value pair based "
+        "configuration for Python applications."
+    ),
     long_description=readme(),
     long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    url="https://github.com/RobertoPrevato/roconfiguration",
+    url="https://github.com/Neoteroi/roconfiguration",
     author="RobertoPrevato",
     author_email="roberto.prevato@gmail.com",
-    keywords="configuration core yaml ini json environment",
+    keywords="configuration root core yaml ini json environment",
     license="MIT",
     packages=["roconfiguration"],
-    install_requires=["PyYAML"],
+    install_requires=["PyYAML==5.4.1"],
     include_package_data=True,
     zip_safe=False,
 )
